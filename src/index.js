05cd08873da214cedd035be198940c2e686a979b
RoySegall <roy@segall.io>
2021-03-31 22:17:07 +0300

    Starting to work on the animals

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {getPets, getPetsFromFile} from "./utils/pets";
+
 const express = require('express');
 import {resolve, join} from 'path';
 const app = express();
@@ -8,9 +10,16 @@ app.set('views', resolve('src', 'templates'));
 
 app.use(express.static(join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
+  const pets = getPets({driver: 'files'});
   res.render('index', { title: 'Hessy', message: 'Hello there!' })
 })
+
+app.get('/pet/:id', (req, res) => {
+  const pets = getPets({driver: 'files', excludePet: req.params.id});
+
+  // Get all the pets and exclude on on the left.
+});
 
 app.listen(port, () => {
   console.log(`Running the server at port ${port}`)
--- src/utils/pets.js
+++ src/utils/pets.js
@@ -1,8 +1,26 @@
-export function getPets({driver, excludePet = null}) {
-  // Load the handler by driver.
+import {resolve} from 'path';
 
-  // Get all the pets.
-  const pets = [];
+export function getPetsFromFile() {
+  const petsNames = ['bowtruckle', 'hippogriff', 'niffler', 'puffskein', 'thestral', 'unicorn'];
+  const pets = {};
+
+  petsNames.forEach((name, index) => {
+    pets[index + 1] = require(resolve('src', 'data', `${name}.json`))
+  });
+
+  return pets;
+}
+
+export function getPets({driver, excludePet = null}) {
+
+  // Get the pets by the driver.
+  let pets, current, otherPets;
+
+  if (driver === 'files') {
+    pets = getPetsFromFile();
+  }
+
+  console.log(pets);
 
   // Check if we need to exclude something.
 
